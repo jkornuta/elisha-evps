28b0cb694a0de551e04d319aa10c567debaecd5a
Jeff Kornuta <Jeff@ibb02124.me.gatech.edu>
2012-10-05 14:49:46 -0400

    Declare CS pin to eliminate confusion between LS7366Rs.

--- libraries/Servo/LS7366R.cpp
+++ libraries/Servo/LS7366R.cpp
@@ -57,10 +57,11 @@ void LS7366RClass::setMDR1Reg(uint8_t config_val)
     _send(WRITE_MDR1, config_val, 0);
 }
       
-double LS7366RClass::readPosition()
 double LS7366RClass::readPosition(uint8_t cs_pin)
 {
     uint32_t count;
+    // Know which QD to use!
+    CSpin = cs_pin;
     
     // send read counter command
     count = _transfer( READ_COUNTER );
